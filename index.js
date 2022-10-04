0c4634a31b6c7309518dc02b5cd6beeaae810f47
Rakesh laishetty <rakeshlaishetty123@gmail.com>
2022-10-04 21:59:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -79,14 +79,18 @@ app.use("/fees", fees);
 app.use("/timetable", timetable);
 app.use("/assignments/", assignments);
 
-if (process.env.NODE_ENV === "production") {
-  console.log("production");
-  app.use(express.static("client/build"));
-  const path = require("path");
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client" + "build" + "index.html"));
-  });
-}
+app.use(express.static(path.join(__dirname, "../client/build")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
+// if (process.env.NODE_ENV === "production") {
+//   console.log("production");
+//   const path = require("path");
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.resolve(__dirname, "client" + "build" + "index.html"));
+//   });
+// }
 
 //********************************* SCHEMAS ****************************************** */
 
